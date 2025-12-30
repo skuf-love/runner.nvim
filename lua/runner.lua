a993701ad3453a70df68ccdeca0f691636220aa1
Kostya Malinovskiy <kostya.malinovskiy@gmail.com>
2025-12-30 10:54:42 +0200

    Add function to run current package

--- lua/runner.lua
+++ lua/runner.lua
@@ -11,6 +11,12 @@ local run_test_command = function()
 	local file_dir = vim.fn.expand("%:p:h")
 	return "go test " .. file_dir
 end
+
+local run_package_command = function()
+	local file_dir = vim.fn.expand("%:p:h")
+	return "go run " .. file_dir
+end
+
 local clean_empty_bufs = function()
 	for _, buf in pairs(vim.api.nvim_list_bufs()) do
 		if vim.api.nvim_buf_get_name(buf) == "" and not vim.bo.modified and vim.api.nvim_buf_is_loaded(buf) then
@@ -46,6 +52,22 @@ vim.keymap.set("n", "<leader>tc", function()
 	vim.fn.termopen(full_command)
 	vim.api.nvim_set_current_win(M.orignal_window_id)
 end, { desc = "[t]est [c]urrent file" })
+
+vim.keymap.set("n", "<leader>rc", function()
+	M.orignal_window_id = vim.api.nvim_get_current_win()
+	close_prev_run()
+	local full_command = run_package_command()
+	print("Run command: " .. full_command)
+	vim.cmd("new")
+	M.test_window_id = vim.api.nvim_get_current_win()
+	print("Open window " .. M.test_window_id)
+	M.current_buffer_id = vim.api.nvim_create_buf(false, true)
+	print("Create buffer " .. M.current_buffer_id)
+	vim.api.nvim_set_current_buf(M.current_buffer_id)
+
+	vim.fn.termopen(full_command)
+	vim.api.nvim_set_current_win(M.orignal_window_id)
+end, { desc = "[r]un [c]urrent file" })
 
 vim.keymap.set("n", "<leader>td", function()
 	close_prev_run()
